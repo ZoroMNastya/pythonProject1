c426c84ce6453c164a90d89d688f6e360ee0bab3
ZoroMNastya <156311634+ZoroMNastya@users.noreply.github.com>
2024-02-19 20:14:35 +0400

    Update post.py

--- model/post.py
+++ model/post.py
@@ -2,7 +2,9 @@ from model.user import User
 
 
 class Post:
-
-    def __int__(self, body: str, author: User):
+    def __int__(self, id, body: str, author: User):
+        self.id = id
         self.body = body
         self.author = author
+    def JSON(self):
+        return {'id': self.id, 'body': self.body, 'author': self.author}
